91a57617ec14aa7dd90608f4a6e06e0332cdae37
uehara <uehara@isi.imi.i.u-tokyo.ac.jp>
2026-03-06 18:45:13 +0900

    :+1: added

---
+++ src/library/core/strogatz.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from typing import Callable, List
+
+class dynamics2D(object):
+    def __init__(
+        self, F: list[Callable[[int], int]], xlim: tuple, ylim: tuple, 
+        n_interval: int = 5, fig_scale: float = 1.0
+    ):
+        assert len(F) == 2
+        assert len(xlim) == 2 and len(ylim) == 2
+        self.F = F
+        self.xlim = xlim
+        self.ylim = ylim
+        self.xwidth = xlim[1] - xlim[0]
+        self.ywidth = ylim[1] - ylim[0]
+        self.n_interval = n_interval
+        self.fig_scale = fig_scale
+        self.init_meshgrid()
+        self.get_UV()
+        self.init_axes()
+        
+    def init_axes(self):
+        self.fig, self.ax = plt.subplots(figsize=(self.xwidth*self.fig_scale, self.ywidth*self.fig_scale))
+        self.ax.set_xlim(*self.xlim)
+        self.ax.set_ylim(*self.ylim)
+        self.ax.grid()
+        self.ax.axhline(y=0, color="grey", linewidth=1.8)
+        self.ax.axvline(x=0, color="grey", linewidth=1.8)
+        
+    def init_meshgrid(self):
+        _x = np.linspace(self.xlim[0], self.xlim[1], self.xwidth*self.n_interval)
+        _y = np.linspace(self.ylim[0], self.ylim[1], self.ywidth*self.n_interval)
+        _X, _Y = np.meshgrid(_x, _y)
+        self.XY = np.zeros(shape=(*_X.shape, 2))
+        self.XY[..., 0] = _X
+        self.XY[..., 1] = _Y
+    
+    def get_UV(self):
+        self.UV = np.zeros_like(self.XY)
+        self.UV[..., 0] = self.F[0](self.XY)
+        self.UV[..., 1] = self.F[1](self.XY)
+        
+    def add_direction_field(self):
+        norms = np.linalg.norm(self.UV, axis=-1, keepdims=True)
+        norm_UV = self.UV/norms
+        self.ax.quiver(self.XY[..., 0], self.XY[..., 1], norm_UV[..., 0], norm_UV[..., 1], norms, cmap="viridis")
+    
+    def add_nullclines(self):
+        self.ax.contour(self.XY[..., 0], self.XY[..., 1], self.UV[..., 0], levels=[0], cmap="hsv")
+        self.ax.contour(self.XY[..., 0], self.XY[..., 1], self.UV[..., 1], levels=[0], cmap="hsv")
+    
+    def add_fixed_points(self, fixed_points: list[list], fp_types: list[str], markersize: float = 12):
+        fp = np.array(fixed_points)
+        assert fp.shape[-1] == 2
+        assert fp.shape[0] == len(fp_types)
+        
+        # FIXME: fp_types
+        self.ax.plot(fp[..., 0], fp[..., 1], marker=".", markersize=markersize, color="tab:red")
+        self.ax.plot(fp[..., 0], fp[..., 1], marker=".", markersize=0.5*markersize, color="white")
+        
+    def add_flows(self, initials: list[list]):
+        init_x = np.array(initials)
+        self.ax.streamplot(self.XY[..., 0], self.XY[..., 1], self.UV[..., 0], self.UV[..., 1], start_points=init_x, color='tab:blue', linewidth=2)
